b25ba61f5da62e05e9070da84327372aba92a07b
Max Yeo <mzwyeo@gmail.com>
2014-09-06 15:27:01 -0400

    relative paths

--- src/trackMotion.py
+++ src/trackMotion.py
@@ -10,7 +10,7 @@ import os, sys, inspect, thread, time
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 arch_dir = '../lib/win/x64' if sys.maxsize > 2**32 else '../lib/win/x86'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
-sys.path.insert(0,"E:\users\Eric\Documents\GitHub\ALS-Leap-Challenge\lib\win")
+sys.path.insert(0, arch_dir)
 
 import Leap, sys, thread, time
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
